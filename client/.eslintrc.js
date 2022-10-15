812aea212c70c526f85a25da3478fefbc4105c34
disVill <konokou2000@gmail.com>
2022-10-15 10:34:02 +0900

    linter調整

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,24 +1,65 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+    ecmaVersion: 12,
+  },
+  plugins: ['react', '@typescript-eslint', 'import', 'unused-imports'],
+  rules: {
+    'react/prop-types': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'unused-imports/no-unused-imports': 'warn',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'never',
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'builtin',
+            posigion: 'before',
+          },
+          {
+            pattern:
+              '{api,api/**,assets,assets/**,components/**,hooks/**,lib/**,pages/**,routes,theme,utils,utils/**}',
+            group: 'internal',
+            posigion: 'before',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
     ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+    'import/no-duplicates': 'warn',
+  },
 }
