5fc646cafcf43f8ddcee2085a3a38c6f70492820
datesann0109 <t118053.kushima@gmail.com>
2022-10-16 15:49:37 +0900

    同じ名称が多かったので短縮化

--- server/database.py
+++ server/database.py
@@ -1,9 +1,8 @@
-import settings
+import settings as s
 
 
 def setup_db(app):
     app.config[
         "SQLALCHEMY_DATABASE_URI"
-    ] = f"mysql://{settings.MYSQL_USER}:{settings.MYSQL_PASSWORD}@{settings.MYSQL_HOST}:{settings.MYSQL_PORT}/\
-        {settings.MYSQL_DATABASE}?charset=utf8"
+    ] = f"mysql://{s.MYSQL_USER}:{s.MYSQL_PASSWORD}@{s.MYSQL_HOST}:{s.MYSQL_PORT}/{s.MYSQL_DATABASE}?charset=utf8"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
