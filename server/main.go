e21a6552fafce6b01ad0a21680b0f191b73d0e59
datesann0109 <t118053.kushima@gmail.com>
2022-10-16 12:29:20 +0900

    /health でヘルスチェックを実装

--- server/main.go
+++ server/main.go
@@ -4,38 +4,32 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
+	"net"
+	"os"
 
-	"golang.org/x/sync/errgroup"
+	"github.com/jphacks/C_2204/config"
 )
 
 func main() {
 	if err := run(context.Background()); err != nil {
-		log.Printf("failed to terminate server: %v", err)
+		log.Printf("failed to terminated server: %v", err)
+		os.Exit(1)
 	}
 }
 
 func run(ctx context.Context) error {
-	s := &http.Server{
-		Addr: ":4000",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
+	cfg, err := config.New()
+	if err != nil {
+		return err
 	}
-	eg, ctx := errgroup.WithContext(ctx)
-	eg.Go(func() error {
-		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Panicf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	// チャネルからの通知を待機する
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
 
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
