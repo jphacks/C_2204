c88e7af59a01c34521c6ea16b47f0bcfa0b8607e
datesann0109 <t118053.kushima@gmail.com>
2022-10-17 21:20:29 +0900

    500 Internal Server Error用関数を追加

--- server/service/error_response.py
+++ server/service/error_response.py
@@ -2,4 +2,10 @@ from flask import make_response, jsonify
 
 
 def bad_request_response():
+    # 400 Bad Request
     return make_response(jsonify({"code": 400, "status": "Bad Request"})), 400
+
+
+def internal_server_error_response():
+    # 500 Internal Server Error
+    return make_response(jsonify({"code": 500, "status": "Internal Server Error"})), 500
