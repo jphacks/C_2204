a261a09d8d150c6dc4f0ab60140b82202a717faa
datesann0109 <t118053.kushima@gmail.com>
2022-10-18 16:03:50 +0900

    TODOを追加

--- server/service/get_photos_presigned_url_service.py
+++ server/service/get_photos_presigned_url_service.py
@@ -6,6 +6,7 @@ from flask import make_response, jsonify
 
 
 def get_photos_presigned_url_response():
+    # TODO:確率は低いが既に使われているファイル名か確認する必要あり
     key = f"up_img/{uuid.uuid4()}.png"
     url = boto3.client(
         "s3", aws_access_key_id=s.AWS_ACCESS_KEY_ID, aws_secret_access_key=s.AWS_SECRET_ACCESS_KEY
