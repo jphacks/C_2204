6f333de622beb37f74d6bceceeb8b0a2ed2ca654
datesann0109 <t118053.kushima@gmail.com>
2022-10-20 20:49:26 +0900

    サンプルを追加

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,3 +12,24 @@ cd ./C_2204/client/
 yarn install --prod --frozen-lockfile
 yarn build
 yarn start
+
+
+#!/bin/bash
+
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+
+sudo apt update
+
+sudo apt install mysql-server -y
+
+sudo apt install nginx -y
+
+sudo systemctl reload nginx
+
+sudo apt-get install python3-dev default-libmysqlclient-dev build-essential -y
+sudo apt install python3-pip -y
+
+sudo apt install -y nodejs
+sudo npm install -g yarn
+
+pip install -r ./server/requirements.txt
