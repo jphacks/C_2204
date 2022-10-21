36d8ae1a76b115400cf6b0227c627f7f207c77ff
datesann0109 <t118053.kushima@gmail.com>
2022-10-22 03:21:57 +0900

    バグの修正

--- server/service/photos.py
+++ server/service/photos.py
@@ -19,7 +19,8 @@ def __to_json(photo: NbgPhotos):
 
 def get_photos():
     photos = NbgPhotos.get_all()
-    photos_json = random.shuffle(list(map(__to_json, photos)))
+    photos_json = list(map(__to_json, photos))
+    random.shuffle(photos_json)
     return make_response(jsonify(photos_json))
 
 
