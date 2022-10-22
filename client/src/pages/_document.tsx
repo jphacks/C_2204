8ffc68737d0ad3d6394f712edb0fea3259c34c2d
datesann0109 <t118053.kushima@gmail.com>
2022-10-22 11:03:19 +0900

    サイト内にコメント分が入り込む問題を修正

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -10,9 +10,9 @@ export default class Document extends NextDocument {
           <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
           <link rel="manifest" href="/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000" />
-          <link rel="shortcut icon" href="/favicons/favicon.ico" /> //IE8以下にも対応したい場合
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
           <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="msapplication-config" content="/favicons/browserconfig.xml" /> //Windows8/10のスタート画面のピン留め画像に対応したい場合
+          <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
