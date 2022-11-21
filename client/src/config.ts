29acd72e67afd7d8d6581c4854a99d2ea3f06355
datesann0109 <t118053.kushima@gmail.com>
2022-11-21 14:39:57 +0900

    新環境様に修正

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,5 +2,5 @@ export default {
   BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || '',
   S3_IMG_BUCKET_ENDPOINT: process.env.NEXT_PUBLIC_S3_IMG_BUCKET_ENDPOINT || '',
   API_BASE_URL:
-    process.env.NEXT_PUBLIC_API_BASE_URL || 'https://jphacks.datech.jp/api',
+    process.env.NEXT_PUBLIC_API_BASE_URL || 'http://parallel-memory.ls.datech.jp/api',
 }
